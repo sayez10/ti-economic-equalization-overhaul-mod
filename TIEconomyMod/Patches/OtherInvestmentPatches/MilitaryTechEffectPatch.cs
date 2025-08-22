72e0663e77453e0c62852c5f99ad3fc3188e6b1c
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-08-22 20:42:22 +0200

    Remove the population scaling of the Military priority. Instead the effect now scales with the number of existing armies and navies. Ceteris paribus, the more forces a country has, the slower the increase of miltech will be. This is supposed to represent the cost to upgrade or replace equipment and to train personnel. For now the formula used is simplistic: a fixed malus per army or navy, which is capped to avoid reducing the miltech growth to 0 (or below) regardless of investments. This should really be changed, but first I want to see how this feels in a longer game. I also expect progress to be too quick (due to the much higher emount of available IPs in most relevant nations). And it might be a good idea to add a knowledge bonus.

--- TIEconomyMod/Patches/OtherInvestmentPatches/MilitaryTechEffectPatch.cs
+++ TIEconomyMod/Patches/OtherInvestmentPatches/MilitaryTechEffectPatch.cs
@@ -24,18 +24,20 @@ namespace TIEconomyMod
             // If mod has been disabled, abort patch and use original method.
             if (!Main.enabled) { return true; }
 
-            const float BASE_MILTECH = 0.025f;
+            const float BASE_MILTECH = 0.0025f;
             const float MILTECH_PER_MILTECH_LEVEL_BEHIND = 0.5f;
+            const float MILTECH_MALUS_CAP = -0.00125f;
+            const float MILTECH_MALUS_PER_ARMY = -0.001f;
+            const float MILTECH_MALUS_PER_NAVY = -0.0005f;
 
-            // Refer to EffectStrength() comments for explanation.
-            float baseEffect = Tools.EffectStrength(BASE_MILTECH, __instance.population);
+	    float armiesNumberEffect = Mathf.Max(MILTECH_MALUS_CAP, (__instance.numStandardArmies * MILTECH_MALUS_PER_ARMY) + (__instance.numNavies * MILTECH_MALUS_PER_NAVY));
 
             // Additionally, add a catch-up multiplier dependent on how far behind the max tech level the country is
             // A bonus 50% tech gain per full tech level behind the global max
             // Max to 1 is to prevent weirdness if somehow mil tech is above max mil tech
-            float catchUpMult = Mathf.Max(1.0f + (MILTECH_PER_MILTECH_LEVEL_BEHIND * (__instance.maxMilitaryTechLevel - __instance.militaryTechLevel)), 1.0f);
+            float catchUpMult = Mathf.Max(1f, 1f + (MILTECH_PER_MILTECH_LEVEL_BEHIND * (__instance.maxMilitaryTechLevel - __instance.militaryTechLevel)));
 
-            __result = baseEffect * catchUpMult;
+            __result = (BASE_MILTECH + armiesNumberEffect) * catchUpMult;
 
 
             return false; // Skip original method
