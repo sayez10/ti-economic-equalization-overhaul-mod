22ce6a957abd3ff34fb5e454fd27a701675134d9
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-08-24 10:11:16 +0200

    Add a new patch to increase the maximum monthly funding income by approximately a factor of 10 to match the faster increase of the Funding priority and to compensate for the nerf of Spoils

---
+++ TIEconomyMod/Patches/NationalValuesPatches/FundingLimitPatch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using PavonisInteractive.TerraInvicta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+
+namespace TIEconomyMod
+{
+    /// <summary>
+    /// Patch changes the amount of investment points available to a nation to scale linearly with GDP
+    /// </summary>
+    [HarmonyPatch(typeof(TINationState), "maxFunding_year", MethodType.Getter)]
+    public static class FundingLimitPatch
+    {
+        [HarmonyPrefix]
+        public static bool MaxFundingYearOverwrite(ref float __result, TINationState __instance)
+        {
+            // If mod has been disabled, abort patch and use original method
+            if (!Main.enabled) { return true; }
+
+            const double ANNUAL_FUNDING_PER_GDP = 0.00000005;
+
+            __result = (float)(ANNUAL_FUNDING_PER_GDP * __instance.GDP);
+
+
+            return false; // Skip original method
+        }
+    }
+}
