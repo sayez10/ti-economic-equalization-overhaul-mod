ca364e8a63074dc2cabdac550b049c5debf8cf14
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-08-30 04:32:57 +0200

    Clean up SpoilsEffectsPatch.cs and UnityEffectsPatch.cs (which consisted mostly of copied disassembled code, the only changes being the handling of propaganda effects)

--- TIEconomyMod/Patches/SpoilsInvestmentPatches/SpoilsEffectsPatch.cs
+++ TIEconomyMod/Patches/SpoilsInvestmentPatches/SpoilsEffectsPatch.cs
@@ -6,8 +6,6 @@ using System;
 using HarmonyLib;
 using PavonisInteractive.TerraInvicta;
 
-using static PavonisInteractive.TerraInvicta.TINationState;
-
 
 
 namespace TIEconomyMod
@@ -16,6 +14,8 @@ namespace TIEconomyMod
     /// Patch changes the effects of the spoils investment
     /// This overwrite is necessary to fix the propoganda effect, which would otherwise be far too powerful
     /// Otherwise, this method is almost as vanilla, barring referenced values that are changed in other patches
+    ///
+    /// nameof() can't be used here because OnSpoilsPriorityComplete is private in vanilla
     /// </summary>
     [HarmonyPatch(typeof(TINationState), "OnSpoilsPriorityComplete")]
     internal static class SpoilsEffectsPatch
@@ -27,40 +27,39 @@ namespace TIEconomyMod
             if (!Main.enabled) { return true; }
 
             // As vanilla
-            __instance.AddToInequality(__instance.spoilsPriorityInequalityChange, InequalityChangeReason.InqReason_SpoilsPriority);
-            __instance.AddToDemocracy(__instance.spoilsPriorityDemocracyChange, DemocracyChangeReason.DemReason_SpoilsPriority);
-            TIFactionState controlPointTypeOwner = __instance.GetControlPointTypeOwner(ControlPointType.Aristocracy);
-            TIFactionState controlPointTypeOwner2 = __instance.GetControlPointTypeOwner(ControlPointType.ExtractiveSector);
+            __instance.AddToInequality(__instance.spoilsPriorityInequalityChange, TINationState.InequalityChangeReason.InqReason_SpoilsPriority);
+            __instance.AddToDemocracy(__instance.spoilsPriorityDemocracyChange, TINationState.DemocracyChangeReason.DemReason_SpoilsPriority);
+            __instance.AddToSustainability(__instance.spoilsSustainabilityChange);
 
-            foreach (TIControlPoint controlPoint in __instance.controlPoints)
+            // Greenhouse Gases
+            TIGlobalValuesState.GlobalValues.AddSpoilsPriorityEnvEffect(__instance, 0f);
+
+            // Bonus money for owners of aristocracy and extractive sector control points (if they exist in this nation)
+            TIFactionState aristocracyCPOwner = __instance.GetControlPointTypeOwner(ControlPointType.Aristocracy);
+            TIFactionState extractiveSectorCPOwner = __instance.GetControlPointTypeOwner(ControlPointType.ExtractiveSector);
+
+            foreach (TIControlPoint iControlPoint in __instance.controlPoints)
             {
-                float num = __instance.spoilsPriorityMoneyPerControlPoint * ((controlPointTypeOwner == controlPoint.faction) ? TemplateManager.global.aristoracySpoilsMult : 1f) + ((controlPointTypeOwner2 == controlPoint.faction) ? (TemplateManager.global.extractiveSpoilsBonusPerResourceRegion * (float)__instance.currentResourceRegions) : 0f);
-                num += TIEffectsState.SumEffectsModifiers(Context.SpoilsOutput, controlPoint.faction, num);
-                if (controlPoint.faction != null && !controlPoint.benefitsDisabled)
+                float moneyIncome = __instance.spoilsPriorityMoneyPerControlPoint * ((aristocracyCPOwner == iControlPoint.faction) ? TemplateManager.global.aristoracySpoilsMult : 1f);
+                moneyIncome += (extractiveSectorCPOwner == iControlPoint.faction) ? (TemplateManager.global.extractiveSpoilsBonusPerResourceRegion * (float)__instance.currentResourceRegions) : 0f;
+                moneyIncome += TIEffectsState.SumEffectsModifiers(Context.SpoilsOutput, iControlPoint.faction, moneyIncome);
+
+                if (iControlPoint.faction != null && !iControlPoint.benefitsDisabled)
                 {
-                    controlPoint.faction.AddToCurrentResource(num, FactionResource.Money, suppressFactionResourcesUpdatedEvent: false, "Spoils");
-                    controlPoint.faction.thisWeeksCumulativeSpoils += num;
-                    controlPoint.faction.thisMonthsCumulativeSpoils += num;
+                    iControlPoint.faction.AddToCurrentResource(moneyIncome, FactionResource.Money, suppressFactionResourcesUpdatedEvent: false, "Spoils");
+                    iControlPoint.faction.thisWeeksCumulativeSpoils += moneyIncome;
+                    iControlPoint.faction.thisMonthsCumulativeSpoils += moneyIncome;
                 }
             }
 
-            /*
-             * Propaganda Effect:
-             *
-             * Neither I nor the original author yet understand what's going on with propaganda enough to understand how it scales with population.
-             * The original author disabled it, but I didn't like that.
-             */
-            const float BASE_PROPAGANDA = -0.1f;
+            // FIXME: Propaganda effect might need to be re-balanced. Scaling with population?
+            const float BASE_PROPAGANDA = -0.125f;
             float propagandaEffect = (__instance.education + __instance.democracy) * BASE_PROPAGANDA;
 
-            foreach (TIFactionState item in __instance.FactionsWithControlPoint)
+            foreach (TIFactionState iFaction in __instance.FactionsWithControlPoint)
             {
-                __instance.PropagandaOnPop_PerOwnedCPFraction(item.ideology, propagandaEffect);
+                __instance.PropagandaOnPop_PerOwnedCPFraction(iFaction.ideology, propagandaEffect);
             }
-
-            // As vanilla
-            __instance.AddToSustainability(__instance.spoilsSustainabilityChange);
-            TIGlobalValuesState.GlobalValues.AddSpoilsPriorityEnvEffect(__instance, __instance.priorityEffectPopScaling * __instance.sustainability);
 
 
             return false; // Skip original method
--- TIEconomyMod/Patches/UnityInvestmentPatches/UnityEffectsPatch.cs
+++ TIEconomyMod/Patches/UnityInvestmentPatches/UnityEffectsPatch.cs
@@ -6,8 +6,6 @@ using System;
 using HarmonyLib;
 using PavonisInteractive.TerraInvicta;
 
-using static PavonisInteractive.TerraInvicta.TINationState;
-
 
 
 namespace TIEconomyMod
@@ -26,30 +24,20 @@ namespace TIEconomyMod
             // If mod has been disabled, abort patch and use original method
             if (!Main.enabled) { return true; }
 
-            /*
-             * Propaganda Effect:
-             *
-             * As with the Spoils propoganda effect, it's unfortunately beyond me to understand this well enough to get things where I want them
-             * For now, the best way for me to handle this issue is to simply disable the system entirely, pending additional research and math
-             * For now, I'm tacking on a x0.2 multiplier until I can understand what's going on there.
-             *
-             * FIXME: Change this once I understand PropagandaOnPop
-             */
-
-            // Strength is multiplied by 0.2, to account for much higher IP
+            // FIXME: Propaganda effect might need to be re-balanced. Scaling with population?
+            // Strength is multiplied by 0.2 for now, to account for much higher IP and more priority completions
             const float BASE_PROPAGANDA = 0.2f;
             float propagandaEffect = TemplateManager.global.unityPublicOpinionBaseStrength * __instance.priorityEffectPopScaling * BASE_PROPAGANDA;
-            TIFactionState controlPointOfTypeFaction = __instance.GetControlPointOfTypeFaction(ControlPointType.Religion);
+            TIFactionState religionCPOwner = __instance.GetControlPointOfTypeFaction(ControlPointType.Religion);
 
-            // As vanilla
-            foreach (TIFactionState item in __instance.FactionsWithControlPoint)
+            foreach (TIFactionState iFaction in __instance.FactionsWithControlPoint)
             {
-                __instance.PropagandaOnPop_PerOwnedCP(item.ideology, propagandaEffect, (controlPointOfTypeFaction == item) ? TemplateManager.global.religionUnityPublicOpinionBonusStrength : 0);
+                __instance.PropagandaOnPop_PerOwnedCP(iFaction.ideology, propagandaEffect, (religionCPOwner == iFaction) ? TemplateManager.global.religionUnityPublicOpinionBonusStrength : 0);
             }
 
             // As vanilla
-            __instance.AddToCohesion(__instance.unityPriorityCohesionChange, CohesionChangeReason.CohesionReason_UnityPriority);
-            __instance.AddToEducation(__instance.unityPriorityEducationChange, EducationChangeReason.EducationReason_UnityPriority);
+            __instance.AddToCohesion(__instance.unityPriorityCohesionChange, TINationState.CohesionChangeReason.CohesionReason_UnityPriority);
+            __instance.AddToEducation(__instance.unityPriorityEducationChange, TINationState.EducationChangeReason.EducationReason_UnityPriority);
 
 
             return false; // Skip original method
