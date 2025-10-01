6c1e0054f0904154965642ceccfb28dda925d7a5
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-10-01 19:30:48 +0200

    Replace patch of economyScore getter with patch of ModifyGDP() to change monthly IP calculation
    The underlying math wasn't changed and monthly IP generation in nations should not change at all.
    
    But this should be a bit more efficient (only calculate a nation's monthly IP each time it's changed and not each time it's used).
    And IMHO it's a bit more elegant.

---
+++ TIEconomyMod/patches/NationalValuesPatches/EconomyScorePatch.cs
@@ -0,0 +1,37 @@
+﻿// SPDX-FileCopyrightText: Copyright © 2025 sayez10
+//
+// SPDX-License-Identifier: MIT
+
+using System;
+using HarmonyLib;
+using PavonisInteractive.TerraInvicta;
+
+
+
+namespace TIEconomyMod
+{
+    /// <summary>
+    /// Called every time after TINationState.ModifyGDP() completes
+    /// That vanilla method is called every time a nation's GDP changes
+    /// Overwrites the nation's economyScore member (= monthly IP), which had been set during ModifyGDP()
+    /// Instead of GDPInBillions^0.33 like in Vanilla, we use linear scaling
+    /// That can be further modified by a user-defined factor
+    /// </summary>
+    [HarmonyPatch(typeof(TINationState), nameof(TINationState.ModifyGDP))]
+    internal static class EconomyScorePatch
+    {
+        [HarmonyPostfix]
+        private static void ModifyGDPPostfix(TINationState __instance)
+        {
+            // If mod has been disabled, abort patch
+            if (!Main.enabled) { return; }
+
+            // Linear scaling: E.g. 500 billion GDP * 0.000_000_000_01d = 5 IP/month
+            float economyScore = (float)(__instance.GDP * Tools.IPPerGDP);
+
+            Traverse traverse = Traverse.Create(__instance);
+            traverse.Property("economyScore", null).SetValue(economyScore);
+            __instance.SetDataDirty();
+        }
+    }
+}
