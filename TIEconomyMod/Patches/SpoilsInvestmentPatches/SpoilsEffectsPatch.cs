44186950b14fa4865a3e107a32455bd73c7ab5cd
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-10-01 21:10:04 +0200

    Reverted past cleanups of SpoilsEffectsPatch.cs
    Instead I'm trying to make the function be almost identical as the vanilla function.
    The goal is to simplify merging changes to vanilla back into the patched function.
    The only significant exception is the code block for the propaganda effect.
    
    All changes from vanilla have been marked with comments.

--- TIEconomyMod/Patches/SpoilsInvestmentPatches/SpoilsEffectsPatch.cs
+++ TIEconomyMod/Patches/SpoilsInvestmentPatches/SpoilsEffectsPatch.cs
@@ -12,7 +12,7 @@ namespace TIEconomyMod
 {
     /// <summary>
     /// Patch changes the effects of the spoils investment
-    /// This overwrite is necessary to fix the propoganda effect, which would otherwise be far too powerful
+    /// This overwrite is necessary to change the propoganda effect, which would otherwise be far too powerful
     /// Otherwise, this method is almost as vanilla, barring referenced values that are changed in other patches
     ///
     /// nameof() can't be used here because OnSpoilsPriorityComplete is private in vanilla
@@ -26,29 +26,21 @@ namespace TIEconomyMod
             // If mod has been disabled, abort patch and use original method
             if (!Main.enabled) { return true; }
 
-            // As vanilla
+            // Same as vanilla
             __instance.AddToInequality(__instance.spoilsPriorityInequalityChange, TINationState.InequalityChangeReason.InqReason_SpoilsPriority);
             __instance.AddToDemocracy(__instance.spoilsPriorityDemocracyChange, TINationState.DemocracyChangeReason.DemReason_SpoilsPriority);
-            __instance.AddToSustainability(__instance.spoilsSustainabilityChange);
+            TIFactionState controlPointTypeOwner = __instance.GetControlPointTypeOwner(ControlPointType.Aristocracy);
+            TIFactionState controlPointTypeOwner2 = __instance.GetControlPointTypeOwner(ControlPointType.ExtractiveSector);
 
-            // Greenhouse Gases
-            TIGlobalValuesState.GlobalValues.AddSpoilsPriorityEnvEffect(__instance, 0f);
-
-            // Bonus money for owners of aristocracy and extractive sector control points (if they exist in this nation)
-            TIFactionState aristocracyCPOwner = __instance.GetControlPointTypeOwner(ControlPointType.Aristocracy);
-            TIFactionState extractiveSectorCPOwner = __instance.GetControlPointTypeOwner(ControlPointType.ExtractiveSector);
-
-            foreach (TIControlPoint iControlPoint in __instance.controlPoints)
+            foreach (TIControlPoint ticontrolPoint in __instance.controlPoints)
             {
-                float moneyIncome = __instance.spoilsPriorityMoneyPerControlPoint * ((aristocracyCPOwner == iControlPoint.faction) ? TemplateManager.global.aristoracySpoilsMult : 1f);
-                moneyIncome += (extractiveSectorCPOwner == iControlPoint.faction) ? (TemplateManager.global.extractiveSpoilsBonusPerResourceRegion * (float)__instance.currentResourceRegions) : 0f;
-                moneyIncome += TIEffectsState.SumEffectsModifiers(Context.SpoilsOutput, iControlPoint.faction, moneyIncome);
-
-                if (iControlPoint.faction != null && !iControlPoint.benefitsDisabled)
+                float num = __instance.spoilsPriorityMoneyPerControlPoint * ((controlPointTypeOwner == ticontrolPoint.faction) ? TemplateManager.global.aristoracySpoilsMult : 1f) + ((controlPointTypeOwner2 == ticontrolPoint.faction) ? (TemplateManager.global.extractiveSpoilsBonusPerResourceRegion * (float)__instance.currentResourceRegions) : 0f);
+                num += TIEffectsState.SumEffectsModifiers(Context.SpoilsOutput, ticontrolPoint.faction, num, null);
+                if (ticontrolPoint.faction != null && !ticontrolPoint.benefitsDisabled)
                 {
-                    iControlPoint.faction.AddToCurrentResource(moneyIncome, FactionResource.Money, suppressFactionResourcesUpdatedEvent: false, "Spoils");
-                    iControlPoint.faction.thisWeeksCumulativeSpoils += moneyIncome;
-                    iControlPoint.faction.thisMonthsCumulativeSpoils += moneyIncome;
+                    ticontrolPoint.faction.AddToCurrentResource(num, FactionResource.Money, false, "Spoils");
+                    ticontrolPoint.faction.thisWeeksCumulativeSpoils += num;
+                    ticontrolPoint.faction.thisMonthsCumulativeSpoils += num;
                 }
             }
 
@@ -60,6 +52,10 @@ namespace TIEconomyMod
             {
                 __instance.PropagandaOnPop_PerOwnedCPFraction(iFaction.ideology, propagandaEffect);
             }
+
+            // Same as Vanilla, only replaced a now unused function argument (which contained a multiplication) with 0f as tiny micro-optimization
+            __instance.AddToSustainability(__instance.spoilsSustainabilityChange);
+            TIGlobalValuesState.GlobalValues.AddSpoilsPriorityEnvEffect(__instance, 0f);
 
 
             return false; // Skip original method
