cc7aab32d862e81bd3f0fbddac3f3d66044ef166
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-10-03 18:11:00 +0200

    Rename a few local variables in SpoilsEnvironmentEffectPatch (more descriptive names)

--- TIEconomyMod/patches/investment.spoils/SpoilsEnvironmentEffectPatch.cs
+++ TIEconomyMod/patches/investment.spoils/SpoilsEnvironmentEffectPatch.cs
@@ -27,16 +27,16 @@ namespace TIEconomyMod
             float resourceRegionsMult = 1f + (nation.currentResourceRegions * ENVIRONMENT_MULT_PER_RESOURCE_REGION);
 
             // CO2
-            float effectCO2 = TemplateManager.global.SpoCO2_ppm * resourceRegionsMult;
-            __instance.AddCO2_ppm(effectCO2, GHGSources.SpoilsPriority);
+            float emissionsCO2 = TemplateManager.global.SpoCO2_ppm * resourceRegionsMult;
+            __instance.AddCO2_ppm(emissionsCO2, GHGSources.SpoilsPriority);
 
             // CH4
-            float effectCH4 = TemplateManager.global.SpoCH4_ppm * resourceRegionsMult;
-            __instance.AddCH4_ppm(effectCH4, GHGSources.SpoilsPriority);
+            float emissionsCH4 = TemplateManager.global.SpoCH4_ppm * resourceRegionsMult;
+            __instance.AddCH4_ppm(emissionsCH4, GHGSources.SpoilsPriority);
 
             // N2O
-            float effectN2O = TemplateManager.global.SpoN2O_ppm * resourceRegionsMult;
-            __instance.AddN2O_ppm(effectN2O, GHGSources.SpoilsPriority);
+            float emissionsN2O = TemplateManager.global.SpoN2O_ppm * resourceRegionsMult;
+            __instance.AddN2O_ppm(emissionsN2O, GHGSources.SpoilsPriority);
 
 
             return false; // Skip original method
