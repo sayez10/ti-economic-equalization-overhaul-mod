b48f3f3134c74712f1ff423ae4a96eb86971c6eb
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-10-05 05:37:02 +0200

    Rename DirectInvestmentAnnualLimitPatch to DirectInvestmentAnnualLimitPatch

--- TIEconomicEqualizationOverhaulMod/patches/national.actions/DirectInvestmentAnnualLimitPatch.cs
+++ TIEconomicEqualizationOverhaulMod/patches/national.actions/DirectInvestmentAnnualLimitPatch.cs
@@ -13,11 +13,11 @@ namespace TIEconomicEqualizationOverhaulMod
     /// <summary>
     /// Changes the formula to calculate the maximum annual direct investments in nations
     /// The number has been increased significantly
-    /// The cap scales linearly with a nation's monthly IP, multiplied with a user-definable factor
+    /// The limit scales linearly with a nation's monthly IP, multiplied with a user-definable factor
     /// Every nation also gets a flat amount of 200 to allow substantial direct investments in small and poor nations
     /// </summary>
     [HarmonyPatch(typeof(TINationState), nameof(TINationState.MaxAnnualDirectInvestIPs), MethodType.Getter)]
-    internal static class DirectInvestmentAnnualCapPatch
+    internal static class DirectInvestmentAnnualLimitPatch
     {
         [HarmonyPrefix]
         private static bool GetMaxAnnualDirectInvestIPsOverwrite(ref int __result, in TINationState __instance)
