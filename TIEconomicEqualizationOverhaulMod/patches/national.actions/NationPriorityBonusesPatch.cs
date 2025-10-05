eef082644d366498ed39c3e360157ec2edc5cb81
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-10-05 06:08:14 +0200

    Add overwrite patch for TINationState.NationalPriorityBonuses()
    All priorities related to the military now benefit from mining regions in a nation.
    All other priorities don't benefit from them anymore.

---
+++ TIEconomicEqualizationOverhaulMod/patches/national.actions/NationPriorityBonusesPatch.cs
@@ -0,0 +1,43 @@
+﻿// SPDX-FileCopyrightText: Copyright © 2025 sayez10
+//
+// SPDX-License-Identifier: MIT
+
+using System;
+using HarmonyLib;
+using PavonisInteractive.TerraInvicta;
+
+
+
+namespace TIEconomicEqualizationOverhaulMod
+{
+    /// <summary>
+    /// All priorities related to the military now benefit from mining regions in a nation
+    /// All other priorities don't benefit from them anymore
+    /// </summary>
+    [HarmonyPatch(typeof(TINationState), nameof(TINationState.NationalPriorityBonuses))]
+    internal static class NationPriorityBonusesPatch
+    {
+        [HarmonyPrefix]
+        private static bool NationalPriorityBonusesOverwrite(ref float __result, in TINationState __instance, PriorityType priority)
+        {
+            // If mod has been disabled, abort patch and use original method
+            if (!Main.enabled) { return true; }
+
+            if (priority == PriorityType.Economy)
+            {
+                __result = __instance.restofFederationECOBonus_dailyCache * TemplateManager.global.federationGDPEconomyBonus;
+            }
+            else if (priority >= PriorityType.Military_FoundMilitary)
+            {
+                __result = (float)__instance.numMiningRegions_dailyCache * TemplateManager.global.coreMineralBuildMilitaryModifier;
+            }
+            else
+            {
+                __result = 0f;
+            }
+
+
+            return false; // Skip original method
+        }
+    }
+}
