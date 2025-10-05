5311860720833b12817bf70082eafb1ef759bbb6
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-10-05 03:50:12 +0200

    Minor cleanups in OppressionDemocracyEffectPatch

--- TIEconomicEqualizationOverhaulMod/patches/investment.oppression/OppressionDemocracyEffectPatch.cs
+++ TIEconomicEqualizationOverhaulMod/patches/investment.oppression/OppressionDemocracyEffectPatch.cs
@@ -22,9 +22,8 @@ namespace TIEconomicEqualizationOverhaulMod
             // If mod has been disabled, abort patch and use original method
             if (!Main.enabled) { return true; }
 
-            float BASE_DEMOCRACY_EFFECT = -0.0175f;
+            const float BASE_DEMOCRACY_EFFECT = -0.0175f;
 
-            // About 35% of (base) Government effect
             __result = Tools.EffectStrength(BASE_DEMOCRACY_EFFECT, __instance.population);
 
 
